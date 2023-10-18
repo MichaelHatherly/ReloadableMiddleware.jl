2acb465ee51c2186efc1900456da1f8ce0b0e756
Michael Hatherly <michaelhatherly@gmail.com>
2023-10-18 17:55:51 +0100

    Refactor hot-reloader to use a separate server

--- ext/ReloadableMiddlewareReviseExt.jl
+++ ext/ReloadableMiddlewareReviseExt.jl
@@ -41,108 +41,112 @@ function _updated_mtimes(current::Dict, mods::Vector{Module})
     return false, mtimes
 end
 
-# Revise middleware.
-
-function ReloadableMiddleware._revise_middleware(
-    ::ReloadableMiddleware.ReviseMiddlewareDispatchType,
-    handler,
-)
-    return function (req::HTTP.Request)
-        if !isempty(Revise.revision_queue)
-            try
-                @debug "Running revise"
-                Revise.revise()
-            catch error
-                @error "Revise failed to revise" error
-            end
-        end
-        return Base.invokelatest(handler, req)
-    end
-end
-
 # Hot reloader.
-#
-# The hot reloader is a server sent event stream that triggers a reload of the
-# page when `refresh()` is called. The hot reloader is injected into the
-# response body of the HTML response and uses idiomorph to morph the DOM after
-# fetching the new HTML for the current page.
-#
-# This code is not responsible for checking whether any specific files have
-# changed. That should be handled separately by a file watcher task that
-# then calls `refresh()` when a change is detected.
 
 function ReloadableMiddleware._hot_reloader_middleware(
     ::ReloadableMiddleware.HotReloaderDispatchType,
-    endpoint::AbstractString,
 )
-    @info "Hot reloading enabled"
+    # Where the hot-reloader server will listen.
+    host = HTTP.Sockets.localhost
+    port, socket = HTTP.Sockets.listenany(host, rand(49152:65535))
+    # Make the port more human-readable.
+    port = Int(port)
+    address = "http://$host:$port"
 
+    # Keep track of all the clients that are listening for reload events.
     channels = Channel{Bool}[]
 
+    # Trigger a reload event for all clients.
     function refresh()
         while !isempty(channels)
-            channel = pop!(channels)
-            put!(channel, true)
+            put!(pop!(channels), true)
         end
     end
 
-    idiomorph = BundledWebResources.ResourceRouter(ReloadableMiddleware.Idiomorph)
+    function reload(stream::HTTP.Stream)
+        HTTP.setheader(stream, "Access-Control-Allow-Origin" => "*")
+        HTTP.setheader(stream, "Access-Control-Allow-Methods" => "GET, OPTIONS")
+        HTTP.setheader(stream, "Content-Type" => "text/event-stream")
 
-    function router(handler)
-        function handle(req::HTTP.Request)
-            return _hot_reload_transformer(idiomorph(handler)(req), endpoint)
-        end
-        function (stream::HTTP.Stream)
-            target = HTTP.Messages.target(stream.message)
-            if target == endpoint
-                return _reload!(stream, channels)
-            else
-                return HTTP.streamhandler(handle)(stream)
+        if HTTP.method(stream.message) == "OPTIONS"
+            return nothing
+        else
+            HTTP.setheader(stream, "Content-Type" => "text/event-stream")
+            HTTP.setheader(stream, "Cache-Control" => "no-cache")
+
+            channel = Channel{Bool}(1)
+            push!(channels, channel)
+
+            # Block until the channel gets a value pushed to it from the
+            # refresh function.
+            if take!(channel)
+                @info "sending reload event"
+                try
+                    write(stream, "\ndata: reload\n\n")
+                catch error
+                    # Can fail if the user has reloaded their browser window
+                    # manually. In that case, we just ignore the error.
+                    @debug "failed to send reload event" error
+                end
             end
+            return nothing
         end
     end
 
-    (; router, refresh, stream = true)
-end
+    # Create the router that will handle requests for the reload event stream.
+    router = HTTP.Router()
+    HTTP.register!(router, "/", reload)
 
-function _reload!(stream::HTTP.Stream, channels::Vector{Channel{Bool}})
-    HTTP.setheader(stream, "Access-Control-Allow-Origin" => "*")
-    HTTP.setheader(stream, "Access-Control-Allow-Methods" => "GET, OPTIONS")
-    HTTP.setheader(stream, "Content-Type" => "text/event-stream")
+    # Close the listening socket so that we can reuse the port now.
+    close(socket)
 
-    if HTTP.method(stream.message) == "OPTIONS"
-        return nothing
-    end
+    # Start up a streaming HTTP server to handle requests for the reload event.
+    # TODO: should perhaps have a safe way to close this server when not needed
+    # anymore.
+    server = HTTP.serve!(router, host, port; stream = true)
 
-    HTTP.setheader(stream, "Content-Type" => "text/event-stream")
-    HTTP.setheader(stream, "Cache-Control" => "no-cache")
+    # Provide the bundled idiomorph script as a resource. This is used to do
+    # in-place swapping of the DOM when the page is reloaded rather than
+    # performing a full-page refresh.
+    idiomorph_router = BundledWebResources.ResourceRouter(Idiomorph)
 
-    channel = Channel{Bool}(1)
-    push!(channels, channel)
-
-    if take!(channel)
-        try
-            @info "sending reload event"
-            write(stream, "\ndata: reload\n\n")
-        catch
-            # Can fail if the user has reloaded their browser window manually.
-            # In that case, we just ignore the error.
+    @info "Hot reloading enabled"
+    function middleware(handler)
+        # Ensure we intercept the request to handle requests for our bundled
+        # idiomorph script.
+        wrapped_handler = idiomorph_router(handler)
+        return function (request)
+            response = if isempty(Revise.revision_queue)
+                # When there are no pending revisions, we just run the handler
+                # as normal within the dynamic dispatch caused by
+                # `invokelatest`.
+                wrapped_handler(request)
+            else
+                try
+                    @info "Running revise"
+                    Revise.revise()
+                catch error
+                    @error "Revise failed to revise" error
+                end
+                Base.invokelatest(wrapped_handler, request)
+            end
+            return _insert_hot_reloader_script(response, address)
         end
     end
 
-    return nothing
+    return (; middleware, refresh)
 end
 
 # Injects the hot reloading script into the HTML response. Uses idiomorph to
 # morph the DOM after fetching the new HTML for the current page. A server sent
 # event is used to trigger the reload.
-function _hot_reload_transformer(response::HTTP.Response, endpoint)
+function _insert_hot_reloader_script(response::HTTP.Response, address::AbstractString)
     if !isnothing(findfirst((==)("Content-Type" => "text/html"), response.headers))
         tags = "</head>"
         script = """
-        <script src="$(pathof(ReloadableMiddleware.Idiomorph.idiomorph()))"></script>
+        <script src="$(pathof(Idiomorph.idiomorph()))"></script>
         <script>
-        const evtSource = new EventSource("$endpoint");
+        const evtSource = new EventSource("$address");
         evtSource.onmessage = async function (event) {
             if (event.data === "reload") {
                 const response = await fetch(location.pathname);
@@ -153,10 +157,23 @@ function _hot_reload_transformer(response::HTTP.Response, endpoint)
         </script>
         $tags
         """
-        1
         response.body = codeunits(replace(String(response.body), tags => script))
     end
     return response
+end
+
+module Idiomorph
+
+using BundledWebResources
+
+function idiomorph()
+    @comptime Resource(
+        "https://unpkg.com/idiomorph@0.0.9/dist/idiomorph.min.js";
+        name = "idiomorph.js",
+        sha256 = "b9b33450f762cd8510d70e9c5bc3da74ac38127da4089c1932b5898337d7833b",
+    )
+end
+
 end
 
 end
